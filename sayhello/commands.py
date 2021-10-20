ea027044f4783a7910325afbb7f4fdde36cc8693
LuGuanzong <746722756@qq.com>
2021-10-20 21:14:43 +0800

    提交faker分支

--- sayhello/commands.py
+++ sayhello/commands.py
@@ -1,6 +1,7 @@
 import click
 
 from sayhello import app, db
+from sayhello.models import Message
 
 
 @app.cli.command()
@@ -13,3 +14,26 @@ def initdb(drop):
         click.echo('Drop tables.')
     db.create_all()
     click.echo('Initialized database.')
+
+
+@app.cli.command()
+@click.option('--count', default=20, help='Quartity of messages, default is 20.')
+def forge(count):
+    from faker import Faker
+
+    db.drop_all()
+    db.create_all()
+
+    fake = Faker('zh_CN')
+    click.echo('Working...')
+
+    for i in range(count):
+        message = Message(
+            name=fake.name(),
+            body=fake.sentence(),
+            timestamp=fake.date_time_this_year()
+        )
+        db.session.add(message)
+
+    db.session.commit()
+    click.echo('Created %d fake messages.' % count)
