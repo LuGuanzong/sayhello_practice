e3997789e884bab4b6f500618bfb3c2155a395c3
LuGuanzong <746722756@qq.com>
2021-10-20 21:33:46 +0800

    使用调试工具

--- sayhello/__init__.py
+++ sayhello/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask('sayhello')
 app.config.from_pyfile('settings.py')
@@ -11,5 +12,7 @@ app.jinja_env.lstrip_blocks = True
 db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+toolbar = DebugToolbarExtension(app)
+
 
 from sayhello import views, errors, commands
